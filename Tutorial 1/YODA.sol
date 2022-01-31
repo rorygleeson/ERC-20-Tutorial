6e848348825c7cb3663ba77aac6a3a7ce60db07a
rory gleeson <35155069+rorygleeson@users.noreply.github.com>
2022-01-31 13:30:22 +1100

    Update YODA.sol

--- Tutorial 1/YODA.sol
+++ Tutorial 1/YODA.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract YODAToken is ERC20 {
-    constructor(uint256 initialSupply) public ERC20("YODA Token”, “YODA”) {
+    constructor(uint256 initialSupply) public ERC20("YODAToken", "YODA") {
         _mint(msg.sender, initialSupply);
     }
 }
